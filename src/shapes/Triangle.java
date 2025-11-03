eb3261a371b0862989aae8035d3a3c55b598b331
levinnat000 <Nlevin811@gmail.com>
2025-11-03 11:54:23 -0800

    Triangle

--- src/shapes/Triangle.java
+++ src/shapes/Triangle.java
@@ -4,7 +4,7 @@ class Triangle extends Shape {
     private double myLength;
     private double myWidth;
 
-    public Triangle(double theLength, double theWidth) {
+    Triangle(double theLength, double theWidth) {
         myLength = theLength;
         myWidth = theWidth;
     }
